05ccf1e710341886e23d479a7f2c8166dd8f0b02
Gabriel Molina <gabrielmolina717@gmail.com>
2025-02-27 23:44:54 +0800

    Finished the project Added opacity functionality Added contents in README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+Simple etch-a-sketch website made for The Odin Project: Etch-a-Sketch solutions.
+
+This project have taught me about problem solving, the Document Object Model (DOM) and manipulating the DOM by adding various CSS style elements.
--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 const container = document.querySelector("#container");
 
-function gridGenerator(grids = 16) {
-  for (let i = 0; i < grids; i++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("row-div");
-    container.appendChild(rowDiv);
-
-    for (let j = 0; j < grids; j++) {
-      const columnDiv = document.createElement("div");
-      columnDiv.classList.add("column-div");
-      rowDiv.appendChild(columnDiv);
-    }
-  }
-
-  const columnDivSelect = document.querySelectorAll(".column-div");
-
-  columnDivSelect.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-      if (eraserBtn.textContent === "On") {
-        div.style.backgroundColor = "";
-      } else if (randomizeColorBtn.textContent === "Black") {
-        div.style.backgroundColor = "black";
-      } else {
-        div.style.backgroundColor = randomizeColor();
-      }
-    });
-  });
-}
-
 function promptUser() {
   let numOfGrids = prompt("Input numbers 1-100 only");
 
@@ -56,6 +28,57 @@ changeGridBtn.addEventListener("click", () => {
   promptUser();
 });
 
+const opacityContainer = document.querySelector(".opacity-container");
+const opacityBtn = document.querySelector(".opacity-btn");
+opacityBtn.addEventListener("click", () => {
+  if (opacityBtn.textContent === "Off") {
+    opacityBtn.textContent = "On";
+  } else if (opacityBtn.textContent === "On") {
+    opacityBtn.textContent = "Off";
+  }
+});
+
+function opacityColor(div) {
+  if (div.style.backgroundColor != "black") {
+    div.style.backgroundColor = "black";
+    div.style.opacity = "0.1";
+  }
+
+  let divOpacity = div.style.opacity;
+  switch (divOpacity) {
+    case "":
+      div.style.opacity = "0.1";
+      break;
+    case "0.1":
+      div.style.opacity = "0.2";
+      break;
+    case "0.2":
+      div.style.opacity = "0.3";
+      break;
+    case "0.3":
+      div.style.opacity = "0.4";
+      break;
+    case "0.4":
+      div.style.opacity = "0.5";
+      break;
+    case "0.5":
+      div.style.opacity = "0.6";
+      break;
+    case "0.6":
+      div.style.opacity = "0.7";
+      break;
+    case "0.7":
+      div.style.opacity = "0.8";
+      break;
+    case "0.8":
+      div.style.opacity = "0.9";
+      break;
+    case "0.9":
+      div.style.opacity = "1";
+      break;
+  }
+}
+
 function randomizeColor() {
   let randomColor = Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -70,8 +93,11 @@ const randomizeColorBtn = document.querySelector(".randomize-colors-btn");
 randomizeColorBtn.addEventListener("click", () => {
   if (randomizeColorBtn.textContent === "Black") {
     randomizeColorBtn.textContent = "Random";
+    opacityContainer.style.visibility = "hidden";
+    console.log(opacityContainer);
   } else {
     randomizeColorBtn.textContent = "Black";
+    opacityContainer.style.visibility = "visible";
   }
 });
 
@@ -83,5 +109,39 @@ eraserBtn.addEventListener("click", () => {
     eraserBtn.textContent = "Off";
   }
 });
+
+function gridGenerator(grids = 16) {
+  for (let i = 0; i < grids; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row-div");
+    container.appendChild(rowDiv);
+
+    for (let j = 0; j < grids; j++) {
+      const columnDiv = document.createElement("div");
+      columnDiv.classList.add("column-div");
+      rowDiv.appendChild(columnDiv);
+    }
+  }
+
+  const columnDivSelect = document.querySelectorAll(".column-div");
+
+  columnDivSelect.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      if (eraserBtn.textContent === "On") {
+        div.style.backgroundColor = "";
+      } else if (randomizeColorBtn.textContent === "Black") {
+        if (opacityBtn.textContent === "On") {
+          opacityColor(div);
+        } else {
+          div.style.opacity = "1";
+          div.style.backgroundColor = "black";
+        }
+      } else {
+        div.style.opacity = "1";
+        div.style.backgroundColor = randomizeColor();
+      }
+    });
+  });
+}
 
 gridGenerator();
